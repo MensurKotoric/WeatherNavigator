6acf9e6d46d536454307641fc98ca6e681ae49e6
KotoricMensur <mensur.kotoric@edu.htl-klu.at>
2023-05-26 21:39:01 +0200

    implemented method for html side of weather data

--- html/script.js
+++ html/script.js
@@ -12,3 +12,26 @@ function getCoordinates(){
         longitude.value = data[0].lon;
     });
 }
+function htmlSideOfWeatherData(city,temperature, min, max, windSpeed, precipitation, cloudCoverHigh){
+    const out = document.getElementById('output');
+    let icon = '';
+    if(cloudCoverHigh >= 90){
+        icon = 'styles/thunderstormicon.png';
+    } else if(precipitation >= 5){
+        icon = 'styles/rainyicon.png';
+    } else {
+        icon = 'styles/sunnyicon.png';
+    }
+    out.innerHTML = `
+     <div class="card" style="flex-direction: row">
+          <img class="mt-2 rounded mx-auto d-block card-img-top" src="${icon}" style="width: 300px">
+          <div class="card-body text-center">
+            <h3 class="card-title text-danger">${city}</h3>
+            <h5 class="card-text">Current temperature [°C]: ${temperature}</h5>
+            <h5 class="card-text">Min [°C]: ${min}</h5>
+            <h5 class="card-text">Max [°C]: ${max}</h5>
+            <h5 class="card-text">Wind speed [km/h]: ${windSpeed}</h5>
+            <h5 class="card-text">Precipitation [mm]: ${precipitation}</h5>
+          </div>
+     </div>`;
+}
